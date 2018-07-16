dc24d2f86e92da961a39ddefe2ac88ad833bcce9
Douglas Lima <douglasproglima@gmail.com>
2018-07-16 11:36:35 +0000

    Versão 2.0 API

---
+++ app/controllers/api/v2/escala_trabalho_horarios_controller.rb
@@ -0,0 +1,60 @@
+class Api::V2::EscalaTrabalhoHorariosController < ApplicationController
+
+  before_action :authenticate_with_token!
+  respond_to :json
+
+  def index
+    escala_trabalho_horarios = EscalaTrabalhoHorario.all()
+    render json: { escala_trabalho_horarios: escala_trabalho_horarios }, status: 200
+  end
+
+  #Criar método para passar o id pai(id escala) e retornar todos os horários relacionados.
+  # def horariosEscala
+  #   begin
+  #     escala_trabalho_horario = EscalaTrabalhoHorario.escala_trabalho_id.find(params[:id])
+  #     render json: escala_trabalho_horario, status: 200
+  #   rescue
+  #     head 404
+  #   end
+  # end
+
+  def show
+    begin
+      escala_trabalho_horario = EscalaTrabalhoHorario.find(params[:id])
+      render json: escala_trabalho_horario, status: 200
+    rescue
+      head 404
+    end
+  end
+
+  def create
+    escala_trabalho_horario = EscalaTrabalhoHorario.new(escala_trabalho_horario_params)
+
+    if escala_trabalho_horario.save
+      render json: escala_trabalho_horario, status: 201
+    else
+      render json: { errors: escala_trabalho_horario.errors }, status: 422
+    end
+  end
+
+  def update
+    escala_trabalho_horario = EscalaTrabalhoHorario.find(params[:id])
+
+    if escala_trabalho_horario.update_attributes(escala_trabalho_horario_params)
+      render json: escala_trabalho_horario, status: 200
+    else
+      render json: { errors: escala_trabalho_horario.errors }, status: 422
+    end
+  end
+
+  def destroy
+    escala_trabalho_horario = EscalaTrabalhoHorario.find(params[:id])
+    escala_trabalho_horario.destroy
+    head 204
+  end
+
+  private
+  def escala_trabalho_horario_params
+    params.require(:escala_trabalho_horario).permit(:descricao, :hora_inicio, :hora_fim, :escala_trabalho_id)
+  end
+end
---
+++ app/controllers/api/v2/tipo_contratos_controller.rb
@@ -0,0 +1,46 @@
+class Api::V2::TipoContratosController < ApplicationController
+  before_action :authenticate_with_token!
+  respond_to :json
+
+  def index
+    tipo_contratos = current_user.tipo_contratos
+    # tipo_contratos = TipoContrato.all()
+    render json: { tipo_contratos: tipo_contratos }, status: 200
+  end
+
+  def show
+    tipo_contrato = current_user.tipo_contratos.find(params[:id])
+    render json: tipo_contrato, status: 200
+  end
+
+  def create
+    tipo_contrato = current_user.tipo_contratos.build(tipo_contrato_params)
+
+    if tipo_contrato.save
+      render json: tipo_contrato, status: 201
+    else
+      render json: { errors: tipo_contrato.errors }, status: 422
+    end
+  end
+
+  def update
+    tipo_contrato = current_user.tipo_contratos.find(params[:id])
+
+    if tipo_contrato.update_attributes(tipo_contrato_params)
+      render json: tipo_contrato, status: 200
+    else
+      render json: { errors: tipo_contrato.errors }, status: 422
+    end
+  end
+
+  def destroy
+    tipo_contrato = current_user.tipo_contratos.find(params[:id])
+    tipo_contrato.destroy
+    head 204
+  end
+
+  private
+  def tipo_contrato_params
+    params.require(:tipo_contrato).permit(:descricao, :obs)
+  end
+end
