68125076b44e5e0daaebeef6c40439aec402139d
Douglas Lima <douglasproglima@gmail.com>
2018-07-16 17:58:19 +0000

    Add Serializer - Empresa

---
+++ app/serializers/api/v2/empresa_serializer.rb
@@ -0,0 +1,6 @@
+class Api::V2::EmpresaSerializer < ActiveModel::Serializer
+  attributes :id, :ativo, :razao_social, :nome_fantasia, :email, :telefone_1,
+             :telefone_2, :vlr_hora, :percentual_hora_extra, :percentual_add_noturno
+
+  belongs_to :tipo_contrato
+end
