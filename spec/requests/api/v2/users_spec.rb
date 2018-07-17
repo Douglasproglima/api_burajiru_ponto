70aa17adb89d173018ae12a6122065fa5146881d
Douglas Lima <douglasproglima@gmail.com>
2018-07-17 00:23:22 +0000

    Atualização Users teste API

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -4,12 +4,17 @@ RSpec.describe 'Usuários da API', type: :request do
   #OS TESTES REALIZADOS NESSA ETAPA SÃO NO BANCO DE DADOS.
 
   let!(:user) { create(:user) }
-  let(:user_id) { user.id }
+  # let!(:user_id) { user.id }
+  let!(:auth_data) { user.create_new_auth_token }
+
   let(:headers) do
     {
         'Accept' => 'application/vnd.burajiru_ponto.v2',
         'Content-Type' => Mime[:json].to_s,
-        'Authorization' => user.auth_token
+        # 'Authorization' => user.auth_token,
+        'access-token' => auth_data['access-token'],
+        'client' => auth_data['client'],
+        'uid' => auth_data['uid']
     }
   end
 
@@ -18,14 +23,15 @@ RSpec.describe 'Usuários da API', type: :request do
   before { host! 'api.localhost.test' }
 
   #Verbo GET
-  describe 'GET /users/:id' do
-    before do
-      get "/users/#{user_id}", params: {}, headers: headers
-    end
+  describe 'GET /auth/validate_token' do
 
-    context 'Quando o usuário existir' do
+    context 'Quando o cabeçalho da requisição forem válidos' do
+      before do
+        get '/users/auth/validate_token', params: {}, headers: headers
+      end
+
       it 'Retorna o usuário' do
-        expect(json_body[:data][:id].to_i).to eq(user_id)
+        expect(json_body[:data][:id].to_i).to eq(user.id)
       end
 
       it 'Retorna o código status: 200 OK' do
@@ -33,30 +39,34 @@ RSpec.describe 'Usuários da API', type: :request do
       end
     end
 
-    context 'Quando o usuário não existir' do
-      let(:user_id) {1000}
+    context 'Quando o cabeçalho da requisição não forem válidos' do
+      before do
+        headers['access-token'] = "token_invalido"
+        get '/users/auth/validate_token', params: {}, headers: headers
+      end
 
-      it 'Retorna o código status: 404 ERRO' do
-        expect(response).to have_http_status(404)
+      it 'Retorna o código status: 401 ERRO' do
+        expect(response).to have_http_status(401)
       end
     end
   end
 
   #Verbo POST
-  describe 'POST /users' do
+  describe 'POST /auth' do
     before do
-      post '/users', params: { user: user_params }.to_json, headers: headers
+      # post '/users', params: { user: user_params }.to_json, headers: headers
+      post '/auth', params: user_params.to_json, headers: headers
     end
 
     context 'Quando a requisição passando parâmetros é válida' do
       let(:user_params) { FactoryGirl.attributes_for(:user) }
 
-      it 'Retorna o código status: 201' do
-        expect(response).to have_http_status(201)
+      it 'Retorna o código status: 200' do
+        expect(response).to have_http_status(200)
       end
 
       it 'Retorna JSON com os dados do usuário criado' do
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(json_body[:data][:email]).to eq(user_params[:email])
       end
     end
 
@@ -74,9 +84,9 @@ RSpec.describe 'Usuários da API', type: :request do
   end
 
   #Verbo PUT
-  describe 'PUT /users/:id' do
+  describe 'PUT /auth' do
     before do
-      put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
+      put '/auth', params: user_params.to_json, headers: headers
     end
 
     context 'Quando a requisição passando parâmetros é válida' do
@@ -87,7 +97,7 @@ RSpec.describe 'Usuários da API', type: :request do
       end
 
       it 'Retorna JSON com os dados do usuário atualizado' do
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(json_body[:data][:email]).to eq(user_params[:email])
       end
     end
 
@@ -105,14 +115,15 @@ RSpec.describe 'Usuários da API', type: :request do
   end
 
   #Verbo DELETE
-  describe 'DELETE /users/:id' do
+  describe 'DELETE /auth' do
     before do
       # headers = { 'Accept' => 'application/vnd.burajiru_ponto.v1' }
-      delete "/users/#{user_id}", params: {}, headers: headers
+      # delete "/users/#{user_id}", params: {}, headers: headers
+      delete '/auth', params: {}, headers: headers
     end
 
-    it 'Retorna código status: 204 OK' do
-      expect(response).to have_http_status(204)
+    it 'Retorna código status: 200 OK' do
+      expect(response).to have_http_status(200)
     end
 
     it 'Usuário removido do banco de dados' do
