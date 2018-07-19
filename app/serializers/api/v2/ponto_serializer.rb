a36ede1734342561036ac1de26420f2202c338b2
Douglas Lima <douglasproglima@gmail.com>
2018-07-19 08:59:04 +0000

    Add I18n novo formato DateTime e Time - pt-BR

--- app/serializers/api/v2/ponto_serializer.rb
+++ app/serializers/api/v2/ponto_serializer.rb
@@ -1,5 +1,14 @@
 class Api::V2::PontoSerializer < ActiveModel::Serializer
-  attributes :id, :hora_inicio, :hora_fim, :created_at, :updated_at
+  attributes :id, :hora_inicio, :hora_inicio_to_br,
+             :hora_fim, :hora_fim_to_br, :created_at, :updated_at
+
+  def hora_inicio_to_br
+    I18n.l(object.hora_inicio, format: :time_to_br) if object.hora_inicio.present?
+  end
+
+  def hora_fim_to_br
+    I18n.l(object.hora_fim, format: :time_to_br) if object.hora_fim.present?
+  end
 
   belongs_to :dia_ponto
 end
